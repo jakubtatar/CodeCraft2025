ee8b3a3eaba6ecf5396e7be573ae1f532d658537
Jakub Tatar <jakub.tatar4@gmail.com>
2026-01-29 12:26:33 +0100

    Pridany JS na ucenie sa a HoI

---
+++ JavaScript_01/script.js
@@ -0,0 +1,23 @@
+if (true){
+    //Premmenna existuje len vo vnutri podmienky
+    let cislo = 5
+}
+
+//Premenne
+let vek = 16;              
+let vek2 = 20;
+const PI = 3.14;            
+let meno = "Tomáš";      
+
+//Vypise meno, vek a PI
+console.log("Meno je " + meno + " a má " + vek + " rokov. Hodnota PI je " + PI);
+
+//Zmeni hodnotu veku
+vek = vek2
+
+//Vypise meno, novy vek a PI
+console.log("Meno je " + meno + " a má " + vek + " rokov. Hodnota PI je " + PI);
+
+//Musime zadefinovat premennu a vypiseme ju
+let cislo = 5;
+console.log(cislo);
